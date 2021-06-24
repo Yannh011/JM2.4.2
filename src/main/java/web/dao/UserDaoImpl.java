c06606d67f05d348ca99293662479d2498362c3b
Yannh011 <yannh0101@gmail.com>
2021-06-24 13:27:46 +0300

    JM2.4.2

---
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -0,0 +1,75 @@
+package web.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Repository;
+import web.config.SecurityConfig;
+import web.model.User;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<User> getAllUsers() {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<User> cq = cb.createQuery(User.class);
+        Root<User> rootEntry = cq.from(User.class);
+        CriteriaQuery<User> all = cq.select(rootEntry);
+        TypedQuery<User> allQuery = entityManager.createQuery(all);
+        return allQuery.getResultList();
+    }
+
+    @Override
+    public void add(User user) {
+        user.setPassword(passwordEncoder().encode(user.getPassword()));
+        entityManager.persist(user);
+    }
+
+    @Override
+    public void delete(User user) {
+        entityManager.remove(entityManager.contains(user) ? user : entityManager.merge(user));
+    }
+
+    @Override
+    public void update(User user) {
+        entityManager.merge(user);
+    }
+
+    @Override
+    public User getById(long id) {
+        return entityManager.find(User.class, id);
+    }
+
+    public User getByName(String name) {
+        return entityManager.createQuery("select u FROM User u WHERE u.name = :name", User.class).setParameter("name", name).getSingleResult();
+    }
+
+    public UserDaoImpl() {
+        super();
+    }
+
+//    public static NoOpPasswordEncoder passwordEncoder() {
+//        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+//    }
+
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
+
